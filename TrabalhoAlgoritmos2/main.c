d3a5ea77128ef4f3cb7c5941e3b38c725f3c697d
henrirodfgc <henrirodfgc@gmail.com>
2025-10-02 14:44:15 -0300

    Alterações

--- TrabalhoAlgoritmos2/main.c
+++ TrabalhoAlgoritmos2/main.c
@@ -1,11 +1,15 @@
 #include "controller/cliente_controller.h"
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // =============================
 // PONTO DE ENTRADA DO SISTEMA
 // =============================
 
 int main() {
-    
+    system("chcp 65001");
+    setlocale(LC_ALL, "pt-BR");
     iniciar_sistema();
 
     return 0;
